59d87aa090bcca67774e029b4086406671a640f8
scaevius <luke@lkiwebdesign.com.au>
2018-09-10 14:53:30 +1000

    edited config

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 const dbInfo = {
-  mongoURI: 'mongodb://db_user:db_password@db_host/bd_name',
+  mongoURI: 'mongodb://test_admin:smaki5853@ds141185.mlab.com:41185/scaevius_test',
 };
 
 export default dbInfo;
